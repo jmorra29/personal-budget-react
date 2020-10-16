077ac2f5baf0e21d011e22ce2b962ede90ebb2dc
John Morra <jmorra@uncc.edu>
2020-10-16 18:07:44 -0400

    Brought Personal Budget code in

---
+++ personal-budget-react/src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div>
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
